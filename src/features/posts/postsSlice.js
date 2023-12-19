ffc590c4ab27a0a9da28b2a2e2d347987d09c6db
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2023-12-19 23:35:38 +0100

    added Delete post logic

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -28,8 +28,7 @@ export const addNewPost = createAsyncThunk(
     } catch (err) {
       return err.message;
     }
-  }
-);
+  });
 
 export const updatePost = createAsyncThunk(
     "posts/updatePost",
@@ -40,9 +39,22 @@ export const updatePost = createAsyncThunk(
         return response.data;
       } catch (err) {
         return err.message;
+        //return initialPost;
       }
-    }
-  );
+    });
+
+  export const deletePost = createAsyncThunk(
+    "posts/deletePost",
+    async (initialPost) => {
+        const { id } = initialPost;
+      try {
+        const response = await axios.delete(`${POSTS_URL}/${id}`)
+        if (response?.status === 200) return initialPost;
+        return `${response?.status}: ${response?.statusText}`;
+      } catch (err) {
+        return err.message;
+      }
+    });
 
 const postsSlice = createSlice({
   name: "posts",
@@ -123,11 +135,11 @@ const postsSlice = createSlice({
         action.payload.userId = Number(action.payload.userId);
         action.payload.date = new Date().toISOString();
         action.payload.reactions = {
-          thumbsUp: 0,
-          hooray: 0,
-          heart: 0,
-          rocket: 0,
-          eyes: 0,
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
         };
         console.log(action.payload);
         state.posts.push(action.payload);
@@ -142,6 +154,16 @@ const postsSlice = createSlice({
         action.payload.date = new Date().toISOString();
         const posts = state.posts.filter(post => post.id !== id);
         state.posts = [...posts, action.payload];
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+            console.log('Delete could not complete')
+            console.log(action.payload)
+            return;
+        }
+        const { id } = action.payload;
+        const posts = state.posts.filter(post => post.id !== id);
+        state.posts = posts;
       });
   },
 });
